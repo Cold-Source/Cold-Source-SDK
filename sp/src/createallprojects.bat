aa4bc1540285b1a0b1490c7419e20d6217f6407b
REKIS Xype <hvatitbanitmoiakaunty51@gmail.com>
2026-08-04 23:32:34 +0300

    fix build

--- sp/src/createallprojects.bat
+++ sp/src/createallprojects.bat
@@ -1,3 +1,3 @@
-devtools\bin\vpc.exe /2013 /win32 /sdk2013ce +everything +gamepadui /mksln Everything.sln
+devtools\bin\vpc.exe /2022 /win32 /sdk2013ce +everything +gamepadui /mksln Everything.sln
 
 pause
